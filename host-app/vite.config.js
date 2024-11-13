cfa4a743774f9b89abf70deecc80a8f1af50ec5c
amiramahmoudatef2020 <59775385+amiramahmoudatef2020@users.noreply.github.com>
2024-11-13 15:43:45 +0200

    initial configration

---
+++ host-app/vite.config.js
@@ -0,0 +1,14 @@
+import federation from '@originjs/vite-plugin-federation';
+
+export default {
+  plugins: [
+    federation({
+      name: 'host-app',
+      remotes: {
+        microFrontend1: 'http://localhost:3001/assets/remoteEntry.js',
+        microFrontend2: 'http://localhost:3002/assets/remoteEntry.js',
+        microFrontend3: 'http://localhost:3003/assets/remoteEntry.js',
+      },
+    }),
+  ],
+};
