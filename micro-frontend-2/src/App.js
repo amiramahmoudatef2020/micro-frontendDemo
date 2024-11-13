fb310ecea4e620f1cf0e5f3f45fd197f2620273d
amiramahmoudatef2020 <59775385+amiramahmoudatef2020@users.noreply.github.com>
2024-11-13 15:26:22 +0200

    initial structure

---
+++ micro-frontend-2/src/App.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+function App() {
+  return <div className="text-center text-2xl">Welcome to micro-frontend-2!</div>;
+}
+export default App;
---
+++ micro-frontend-3/src/App.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+function App() {
+  return <div className="text-center text-2xl">Welcome to micro-frontend-3!</div>;
+}
+export default App;
