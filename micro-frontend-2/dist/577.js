a9d40541523bb505968a6eee0916502a9a9a9c25
amiramahmoudatef2020 <59775385+amiramahmoudatef2020@users.noreply.github.com>
2024-11-14 08:56:15 +0200

    solve eager issue

--- micro-frontend-2/dist/577.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmicro_frontend_2=self.webpackChunkmicro_frontend_2||[]).push([[577],{577:(e,t,n)=>{n.r(t),n.d(t,{default:()=>o});var c=n(390),r=n.n(c);const o=()=>r().createElement("div",{className:"text-center text-2xl"},"Welcome to micro-frontend-2!")}}]);
